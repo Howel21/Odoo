0912f4061e34d1bdb74fb40840db9ee3af0d456d
elver <jowelespinal@live.com>
2020-07-21 07:54:43 -0600

    ADD-Partner inheritance

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -27,7 +27,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/openacademy.xml',
-        'views/session.xml'
+        'views/session.xml',
+        'views/partner.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
