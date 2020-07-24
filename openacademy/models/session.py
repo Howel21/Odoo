677e5315b90d3b809a37d819c01321295fa98819
Elver <jowelespinal@live.com>
2020-07-24 00:35:14 +0000

    ADD-Model constraints

--- openacademy/models/session.py
+++ openacademy/models/session.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -44,6 +44,11 @@ class Session(models.Model):
                 },
             }
 
-    
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for r in self:
+            if r.instructor_id and r.instructor_id in r.attendee_ids:
+                raise exceptions.ValidationError("A session's instructor can't be an attendee")
+
 
 
